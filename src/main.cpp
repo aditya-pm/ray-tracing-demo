7096d3d82919b0ce47c2cd1374972508fceafea2
aditya-pm <adityapmenon29@gmail.com>
2026-01-10 23:34:49 +0530

    Add basic window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "raylib.h"
+
 int main() {
-    std::cout << "Time to Ray Trace!" << std::endl;
+    InitWindow(1280, 800, "Ray Tracing Demo");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
